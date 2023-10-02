34f83b52904aee143378c683430575ff415138b4
Rahmanto <rahmanto@outlook.com>
2023-10-02 21:24:38 +0900

    first commit

---
+++ server.py
@@ -0,0 +1,47 @@
+# custom_http_server.py
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import cgi
+
+class CustomRequestHandler(SimpleHTTPRequestHandler):
+    
+    # Override POST method
+    def do_POST(self):
+        # Specify the path where you want to handle POST requests
+        if self.path == 'cat':
+            self.handle_post_request()
+
+        # For all other paths, respond with a 404 Not Found
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'Not Found')
+
+    def handle_post_request(self):
+        content_type, _ = cgi.parse_header(self.headers.get('content-type'))
+        content_length = int(self.headers.get('content-length'))
+        post_data = self.rfile.read(content_length)
+
+        # Handle the POST data here (e.g., save it to a file, process it, etc.)
+        # You can replace the following code with your custom logic.
+        response_message = f'Received POST data: {post_data.decode()}'
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(response_message.encode())
+
+    def handle_get_request(self):
+            # Handle the GET request here (e.g., generate dynamic content)
+            # You can replace the following code with your custom logic.
+            response_message = 'This is a custom response to a GET request.'
+
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(response_message.encode())
+
+if __name__ == '__main__':
+    server_address = ('', 80)  # Change the port if needed
+    httpd = HTTPServer(server_address, CustomRequestHandler)
+    print('Server is running on port 8000...')
+    httpd.serve_forever()
