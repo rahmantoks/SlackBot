9fce528bc69aa47351e565887f89eb2148ba675e
rahmanto@conoha-vps <rahmanto@outlook.com>
2023-10-04 17:13:53 +0900

    working synchronous server

--- server.py
+++ server.py
@@ -20,40 +20,45 @@ class CustomRequestHandler(SimpleHTTPRequestHandler):
         # Specify the path where you want to handle POST requests
         if self.path == '/cat':
             self.animal = Cat()
-            self.handle_post_request()
             self.send_response(200)
             self.end_headers()
+            self.handle_post_request()
 
         elif self.path == '/dog':
             self.animal = Dog()
-            self.handle_post_request()
             self.send_response(200)
             self.end_headers()
+            self.handle_post_request()
+
         # For all other paths, respond with a 404 Not Found
         else:
             self.send_response(404)
             self.end_headers()
-
-    def translate(self,input):
-        result = trans.translate_text(input,target_lang="ja")
-        return result.text
     
     def handle_post_request(self):
-
-        desc_jp = "(以下deepl自動翻訳です)\n\n" + self.translate(self.animal.description())
-        name_jp  = self.translate(self.animal.breedname) + "(" + self.animal.breedname + ")"
-
         length = int(self.headers.get('content-length'))
         field_data = self.rfile.read(length)
         fields = parse_qs(str(field_data,"UTF-8"))
 
         print(fields)
-        slack = Slack(fields)
-        slack.create_payload(name_jp,self.animal.pic)
-        slack.post()
-        # slack.create_reply(name_jp,desc_jp)
-        # slack.post_reply()
+        handle_request(fields,self.animal)
 
+def handle_request(params,animal):
+    desc_jp = "(以下deepl自動翻訳です)\n\n" + translate(animal.description())
+    name_jp  = translate(animal.breedname) + "(" + animal.breedname + ")"
+
+    slack = Slack(params)
+    slack.create_payload(name_jp,animal.pic)
+    slack.post()
+
+    ## TODO ##
+    ## Bot need to be in the channel to see conversation ##
+    # slack.create_reply(name_jp,desc_jp)
+    # slack.post_reply()
+
+def translate(input):
+    result = trans.translate_text(input,target_lang="ja")
+    return result.text
         
 if __name__ == '__main__':
     server_address = ('', 8000)  # Change the port if needed
