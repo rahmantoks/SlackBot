3897eecaf87de679380c61f3a965ccde34175af2
rahmanto@conoha-vps <rahmanto@outlook.com>
2023-10-04 17:55:16 +0900

    working on thread reply

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ from datetime import date
 import deepl
 import configparser
 from urllib.parse import parse_qs
+import threading
 
 config = configparser.ConfigParser()
 config.read('config.ini',encoding='utf-8')
@@ -19,31 +20,34 @@ class CustomRequestHandler(SimpleHTTPRequestHandler):
     def do_POST(self):
         # Specify the path where you want to handle POST requests
         if self.path == '/cat':
-            self.animal = Cat()
             self.send_response(200)
             self.end_headers()
-            self.handle_post_request()
+            self.handle_post_request("cat")
 
         elif self.path == '/dog':
-            self.animal = Dog()
             self.send_response(200)
             self.end_headers()
-            self.handle_post_request()
+            self.handle_post_request("dog")
 
         # For all other paths, respond with a 404 Not Found
         else:
             self.send_response(404)
             self.end_headers()
     
-    def handle_post_request(self):
+    def handle_post_request(self,route):
         length = int(self.headers.get('content-length'))
         field_data = self.rfile.read(length)
         fields = parse_qs(str(field_data,"UTF-8"))
 
-        print(fields)
-        handle_request(fields,self.animal)
+        processing_thread = threading.Thread(target=handle_request(fields,route), args=(fields,route))
+        processing_thread.start()
 
-def handle_request(params,animal):
+def handle_request(params,route):
+    if route == "cat":
+        animal = Cat()
+    elif route == "dog":
+        animal = Dog()
+    
     desc_jp = "(以下deepl自動翻訳です)\n\n" + translate(animal.description())
     name_jp  = translate(animal.breedname) + "(" + animal.breedname + ")"
 
@@ -53,8 +57,8 @@ def handle_request(params,animal):
 
     ## TODO ##
     ## Bot need to be in the channel to see conversation ##
-    # slack.create_reply(name_jp,desc_jp)
-    # slack.post_reply()
+    slack.create_reply(name_jp,desc_jp)
+    slack.post_reply()
 
 def translate(input):
     result = trans.translate_text(input,target_lang="ja")
